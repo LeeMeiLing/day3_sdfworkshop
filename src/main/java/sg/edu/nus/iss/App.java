141e53d8290f2cc7f9f2c64195c3391330b7f68b
LeeMeiLing <leem0060@outlook.com>
2023-01-16 15:30:49 +0800

    added users method

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -2,11 +2,12 @@ package sg.edu.nus.iss;
 
 import java.io.Console;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import javax.naming.directory.DirContext;
 
 /**
  * Hello world!
@@ -21,12 +22,14 @@ public final class App {
      * @param args The arguments of the program.
      */
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
 
-        String dirPath = "\\data2";
+        String dirPath = ".\\data2";
         String fileName = "";
 
-        File newDirectory = new File(dirPath);
+        // instantiating a file/directory object
+        File newDirectory = new File (dirPath);
+
         if (newDirectory.exists()) {
             System.out.println("Directory already exists");
         } else {
@@ -55,40 +58,97 @@ public final class App {
                 }
 
                 case "list":
-                    if(cartItems.size() > 0){
-                        for(String item: cartItems){
-                            System.out.printf(item + "\n");
-                        }
-                    }else{
-                        displayMessage("Your cart is empty");
-                    }
+                    listCart(cartItems);
                     break;
 
                 case "users":
+                    listUsers(dirPath);
                     break;
 
                 default:
                     break;
             }
 
+            String strValue = "";
+
             if (input.startsWith("add")) {
 
                 input = input.replace(",", " ");
-                String strValue = "";
                 Scanner scanner = new Scanner(input.substring(4));
-                while(scanner.hasNext()){
+                while (scanner.hasNext()) {
                     strValue = scanner.next();
                     cartItems.add(strValue);
                 }
             }
 
+            if (input.startsWith("delete")) {
+                cartItems = deleteCartItem(cartItems, input);
+            }
+
+            if(input.startsWith("login")){
+                input = input.replace(",", " ");
+                Scanner scanner = new Scanner(input.substring(6));
+                while(scanner.hasNext()){
+                    fileName = scanner.next();
+                }
+
+                File loginFile = new File (dirPath + File.separator + fileName);
+
+                boolean isCreated = loginFile.createNewFile();
+
+                if (isCreated){
+                    displayMessage("New file created successfully" + loginFile.getCanonicalFile());
+                }else{
+                    displayMessage("File already created");
+                }
+
+            }
 
         }
 
     }
 
-
     public static void displayMessage(String message) {
         System.out.println(message);
     }
+
+    public static void listCart(List<String> cartItems) {
+        if (cartItems.size() > 0) {
+            // for(String item: cartItems){
+            // System.out.printf(item + "\n");
+            // }
+            for (int i = 0; i < cartItems.size(); i++) {
+                System.out.printf("%d: %s\n", i + 1, cartItems.get(i));
+            }
+
+        } else {
+            displayMessage("Your cart is empty");
+        }
+    }
+
+    public static List<String> deleteCartItem(List<String> cartItems, String input) {
+
+        String strValue = "";
+        Scanner scanner = new Scanner(input.substring(6));
+        while (scanner.hasNext()) {
+            strValue = scanner.next();
+            int listItemIndex = Integer.parseInt(strValue);
+
+            if (listItemIndex < cartItems.size()) {
+                cartItems.remove(listItemIndex - 1);
+            } else {
+                displayMessage("Incorrect item index");
+            }
+        }
+        return cartItems;
+    }
+
+    public static void listUsers(String dirPath) {
+        File directoryPath = new File(dirPath);
+        String[] contents = directoryPath.list();
+        for(String file:contents){
+            displayMessage(file);
+        }
+    }
+
 }
